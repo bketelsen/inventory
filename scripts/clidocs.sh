f194321bd04e7480e230bf25d59f392a4f21a203
Brian Ketelsen <bketelsen@gmail.com>
2025-04-10 13:52:59 -0400

    docs: add post-release workflow (#48)

--- scripts/clidocs.sh
+++ scripts/clidocs.sh
@@ -5,11 +5,11 @@ SED="sed"
 if which gsed >/dev/null 2>&1; then
 	SED="gsed"
 fi
-NEXT=`svu n`
+NEXT=`svu`
 wholething="# inventory <small>$NEXT</small>"
 # update this directory to the default value
 # of the `--output` flag on the doc generation command
-# and at the end of the script too 
+# and at the end of the script too
 rm -rf ./docs/inventory*.md
 NOCOLOR=1 go run ./cmd/inventory gendocs
 "$SED" \
