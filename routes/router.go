5f5fff4cd7b124f054589768039296275475f09f
Brian Ketelsen <bketelsen@gmail.com>
2025-04-17 19:57:54 -0400

    media and docs (#72)
    * docs: update screenshot
    
    * docs: update screenshot

--- routes/router.go
+++ routes/router.go
@@ -1,20 +1,15 @@
+// Package routes defines the routes for the web server
 package routes
 
 import (
 	"context"
-	"errors"
-	"fmt"
 
 	"github.com/bketelsen/inventory"
 	"github.com/go-chi/chi/v5"
 )
 
 func SetupRoutes(_ context.Context, router chi.Router, storage inventory.Storage) (err error) {
-	if err := errors.Join(
-		setupIndexRoute(router, storage),
-	); err != nil {
-		return fmt.Errorf("error setting up routes: %w", err)
-	}
+	setupIndexRoute(router, storage)
 
 	return nil
 }
